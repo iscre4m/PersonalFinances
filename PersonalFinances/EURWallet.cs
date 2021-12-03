9459c5980361e8df0675331e915d59a2cc145a03
Dmitry <gadgetmirdima85@gmail.com>
2021-12-03 17:34:48 +0200

    PF-26 Сделал основной функционал

---
+++ PersonalFinances/EURWallet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinances
+{
+    class EURWallet:Wallet
+    {
+        EURWallet(string name)
+        {
+            this.Name = name;
+            this.Currency = "EUR";
+        }
+
+        public override void WithDraw(double sum)
+        {
+            if (this.Balance - sum >= 0)
+            {
+                this.Balance -= sum;
+            }
+        }
+
+        public override void Replenish(double sum)
+        {
+            this.Balance += sum;
+        }
+    }
+}
---
+++ PersonalFinances/Expence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinances
+{
+    class Expence
+    {
+        public string Category { get; set; }
+        public double Sum { get; set; }
+        public string Comment { get; set; }
+
+        public Expence(string category,double sum, string comment)
+        {
+            Category = category;
+            Sum = sum;
+            Comment = comment;
+        }
+    }
+}
