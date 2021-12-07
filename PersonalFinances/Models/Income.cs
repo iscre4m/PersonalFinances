b03a7e0f499839135b570338715aada9698218c3
Dmitry <gadgetmirdima85@gmail.com>
2021-12-07 19:50:08 +0200

    PF-17 Добавить возможность пополнения/снятия средств кошелька

--- PersonalFinances/Models/Income.cs
+++ PersonalFinances/Models/Income.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PersonalFinances
 {
-    class Expense: Operation
-    { 
-
+    class Income:Operation
+    {
         public DateTime Date { get; set; }
         public string WalletName { get; set; }
-        public string Category { get; set; }
         public string Sum { get; set; }
         public string Comment { get; set; }
 
-        public Expense(string walletName, string category,string sum,DateTime date,string comment)
+        public Income(string walletName, string sum, DateTime date, string comment)
         {
             WalletName = walletName;
-            Category = category;
             Sum = sum;
             Date = date;
             Comment = comment;
@@ -22,7 +23,7 @@ namespace PersonalFinances
 
         public override string ToString()
         {
-            return WalletName + "\t" + Sum +Category+"\t" +"\t" + Date.Day.ToString() + "." + Date.Month.ToString()
+            return WalletName + "\t" + Sum + "\t" + Date.Day.ToString() + "." + Date.Month.ToString()
                 + Date.Year.ToString() + "\t" + Comment;
         }
     }
--- PersonalFinances/ViewModels/OperationsViewModel.cs
+++ PersonalFinances/ViewModels/OperationsViewModel.cs
@@ -1,29 +1,141 @@
 ﻿using System.Windows.Input;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System;
 
 namespace PersonalFinances
 {
     class OperationsViewModel:Notifier
     {
         WalletsModel walletsModel;
+        ObservableCollection<Operation> operations = new ObservableCollection<Operation>();
         int selectedWalletIndex = -1;
+        int selectedCategoryIndex = -1;
+        int selectedOperationIndex = -1;
+        string[] category = { "Продукты", "Кино", "Развлечения" };
         ICommand replenishCommand;
         ICommand withdrawCommand;
+        ICommand addCommand;
         public OperationsViewModel()
         {
-            Title = "";
-            Currency = "";
-            Balance = "0";
+            IncomeBalance = "0";
+            ExpenseBalance = "0";
         }
-
-        public string Title { get; set; }
-        public string Currency { get; set; }
-        public string Balance { get; set; }
+        public string IncomeBalance { get; set; }
+        public string ExpenseBalance { get; set; }
         public WalletsModel WalletsModel
         {
+            set
+            {
+                walletsModel = value;
+            }
             get
             {
                 return walletsModel;
             }
+        }
+
+        public ObservableCollection<Operation> Operations
+        {
+            get
+            {
+                return operations;
+            }
+        }
+
+        public string[] Category
+        {
+            get
+            {
+                return category;
+            }
+        }
+        public int SelectedWalletIndex
+        {
+            get
+            {
+                return selectedWalletIndex;
+            }
+            set
+            {
+                if (value != selectedWalletIndex)
+                {
+                    selectedWalletIndex = value;
+                    OnPropertyChanged("SelectedWallet");
+                }
+            }
+        }
+        public int SelectedOperationIndex
+        {
+            get
+            {
+                return selectedOperationIndex;
+            }
+            set
+            {
+                if (value != selectedOperationIndex)
+                {
+                    selectedOperationIndex = value;
+                    OnPropertyChanged("SelectedOperation");
+                }
+            }
+        }
+
+        public int SelectedCategoryIndex
+        {
+            get
+            {
+                return selectedCategoryIndex;
+            }
+            set
+            {
+                if (value != selectedCategoryIndex)
+                {
+                    selectedCategoryIndex = value;
+                    OnPropertyChanged("SelectedCategory");
+                }
+            }
+        }
+        Regex balanceRegEx = new Regex(@"(^0|^[1-9][0-9]*)(\.(0[1-9]|[1-9][0-9]?))?$");
+        public ICommand ReplenishCommand
+        {
+            get
+            {
+                if (replenishCommand == null)
+                {
+                    replenishCommand = new DelegateCommand(param => Replenish(),
+                                                     param => (SelectedWalletIndex != -1
+                                                                 && balanceRegEx.IsMatch(IncomeBalance)));
+                }
+                return replenishCommand;
+            }
+        }
+        void Replenish()
+        {
+            walletsModel.Wallets[SelectedWalletIndex].Replenish(Double.Parse(IncomeBalance));
+        }
+
+        void AddOperation()
+        {
+            //operations.Add(new Operation())
+        }
+        public ICommand ExpenseCommand
+        {
+            get
+            {
+                if (withdrawCommand == null)
+                {
+                    withdrawCommand = new DelegateCommand(param => Withdraw(),
+                                                        param => (SelectedWalletIndex != -1
+                                                                 && balanceRegEx.IsMatch(IncomeBalance)));
+                }
+                return withdrawCommand;
+            }
+        }
+
+        void Withdraw()
+        {
+            walletsModel.Wallets[selectedWalletIndex].Withdraw(Double.Parse(ExpenseBalance));
         }
     }
 }
